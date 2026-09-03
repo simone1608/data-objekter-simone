f335d6a3361df486e13c2c9a175c17c26167dd57
Simone <simonesnehvide@gmail.com>
2026-09-03 10:51:46 +0200

    Tilføjede opgave 2

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+// Opgave 1
+// const person = {
+//     firstname: "Simone",
+//     lastname: "Slagslunde",
+//     age: 20,
+//     hometown: "Vanløse",
+//     pet: "Kat",
+//     petname: "Snehvide",
+// }
+
+// console.log(person.firstname);
+// console.log(person.lastname);
+// console.log(person.age);
+
+
+// console.log(person["hometown"]);
+// console.log(person["pet"]);
+// console.log(person["petname"]);
+
+
+// Opgave 2
 const person = {
     firstname: "Simone",
     lastname: "Slagslunde",
+    married: false,
     age: 20,
     hometown: "Vanløse",
-    pet: "Kat",
-    petname: "Snehvide",
-}
+    hobbies: ["Singing", "Watching Series", "BL", "Gaming"],
+    pet: {
+        type: "Kat",
+        name: "Snehvide",
+    },
+};
 
-console.log(person.firstname);
-console.log(person.lastname);
-console.log(person.age);
+person.hobbies.forEach(function (hobby) {
+    console.log(hobby);
+})
 
-
-console.log(person["hometown"]);
-console.log(person["pet"]);
-console.log(person["petname"]);
-
-
-
-
+console.log(person.married);
+console.log(person.pet.type);
+console.log(person.pet.name);
