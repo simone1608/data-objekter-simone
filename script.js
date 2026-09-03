04406ece01897cdac3b92db5a0213fda1144efd8
Simone <simonesnehvide@gmail.com>
2026-09-03 10:36:09 +0200

    Oprettede begge dokumenter og lavet starten

---
+++ script.js
@@ -0,0 +1,21 @@
+const person = {
+    firstname: "Simone",
+    lastname: "Slagslunde",
+    age: 20,
+    hometown: "Vanløse",
+    pet: "Kat",
+    petname: "Snehvide",
+}
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+
+
+console.log(person["hometown"]);
+console.log(person["pet"]);
+console.log(person["petname"]);
+
+
+
+
